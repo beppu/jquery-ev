853a8507a7fc4502d01a1388bcbd8794aabdb121
John Beppu <john.beppu@gmail.com>
2009-09-07 03:50:15 -0700

    allow $.ev.run to be overridden for those who want more control; loop method signature changed significantly

--- jquery.ev.js
+++ jquery.ev.js
@@ -1,55 +1,93 @@
+/* Title: jQuery.ev
+ *
+ * A COMET event loop for jQuery
+ *
+ * $.ev.loop long-polls on a URL and expects to get an array of JSON-encoded
+ * objects back.  Each of these objects should represent a message from the COMET
+ * server that's telling your client-side Javascript to do something.
+ *
+ */
 (function($){
 
-  $.ev = $.ev || {
+  $.ev = {
 
     handlers : {},
     running  : false,
     xhr      : null,
     verbose  : true,
+    timeout  : null,
 
-    run: function(events) {
-      var i;
-      for (i = 0; i < events.length; i++) {
-        var e = events[i];
-        if (!e) continue;
-        var h = this.handlers[e.type];
-        if (h) h(e);
+    /* Method: run
+     *
+     * Respond to an array of messages using the object in this.handlers
+     *
+     */
+    run: function(messages) {
+      var i, m, h; // index, event, handler
+      for (i = 0; i < messages.length; i++) {
+        m = messages[i];
+        if (!m) continue;
+        h = this.handlers[m.type];
+        if (!h) h = this.handlers['*'];
+        if ( h) h(m);
       }
     },
 
+    /* Method: stop
+     *
+     * Stop the loop
+     *
+     */
     stop: function() {
-      this.running = false;
       if (this.xhr) {
         this.xhr.abort();
         this.xhr = null;
       }
-      // Maybe it should let the server side know that it stopped listening.
+      this.running = false;
     },
 
-    loop: function(url, channels) {
+    /*
+     * Method: loop
+     *
+     * Long poll on a URL
+     *
+     * Arguments:
+     *
+     *   url
+     *   handler
+     *
+     */
+    loop: function(url, handlers) {
       var self = this;
+      if (handlers) {
+        if (typeof handlers == "object") {
+          this.handlers = handlers;
+        } else if (typeof handlers == "function") {
+          this.run = handlers;
+        } else {
+          throw("handlers must be an object or function");
+        }
+      }
       this.running = true;
-      if (!channels) channels = [];
       this.xhr = $.ajax({
         type     : 'GET',
         dataType : 'json',
         url      : url,
-        data     : { channels: channels },
-        success  : function(events, status) {
-          // self.log('success', events);
-          self.run(events)
+        timeout  : self.timeout,
+        success  : function(messages, status) {
+          // console.log('success', messages);
+          self.run(messages)
         },
         complete : function(xhr, status) {
           var delay;
           if (status == 'success') {
             delay = 100;
           } else {
-            // self.log('status: ' + status, '; waiting before long-polling again...');
+            // console.log('status: ' + status, '; waiting before long-polling again...');
             delay = 5000;
           }
-          window.setTimeout(function(){
-            if (self.running) self.loop(url, channels);
-          }, delay);
+          // "recursively" loop
+          window.setTimeout(function(){ if (self.running) self.loop(url); }, delay);
         }
       });
     }
